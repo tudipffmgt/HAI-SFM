876f82fe35c061b1deec09d48557ab3ec3a0b653
tudipffmgt <ferdinand.maiwald@tu-dresden.de>
2023-05-12 15:00:02 +0200

    Need to test while loop in downsample_images.py

--- image_processing.py
+++ image_processing.py
@@ -70,9 +70,37 @@ def downsample_images(input_dir, output_dir, image_list):
             return ext
 
     else:
-        # TODO fix when image list is here
 
+        _, ext = os.path.splitext(image_list[0])
 
+        # Process each image file.
+        for image_path in image_list:
+            # Downsample the image.
+            print(f'Downsampling {image_path}...')
+
+            # Load the input image.
+            img = cv2.imread(image_path)
+
+            # Determine the longer edge of the image.
+            height, width = img.shape[:2]
+            if height >= width:
+                longer_edge = height
+            else:
+                longer_edge = width
+
+            # Downsample the image if necessary.
+            if longer_edge > 1600:
+                downsample_factor = longer_edge / 1600
+                new_height, new_width = int(height / downsample_factor), int(width / downsample_factor)
+                img = cv2.resize(img, (new_width, new_height))
+
+            # Construct the output file path.
+            output_path = os.path.join(output_dir, os.path.splitext(os.path.basename(image_path))[0] + '.jpg')
+
+            # Save the downscaled image to the output directory.
+            cv2.imwrite(output_path, img)
+
+    return ext
 
 
 def rotate_images(input_dir, image_tracks, ext):
